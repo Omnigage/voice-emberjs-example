4116cc73584b2f7f889b10819e0c94d463fc9bfe
Aaron Fischer <aaron.fischer@caxiam.com>
2018-02-21 15:30:28 -0500

    Setup index controller and template, renders account dropdown and form for making a call

---
+++ app/controllers/index.js
@@ -0,0 +1,85 @@
+import { computed, observer, get } from '@ember/object';
+import Controller from '@ember/controller';
+import DS from 'ember-data';
+import config from 'voice-example-emberjs/config/environment';
+
+export default Controller.extend({
+  sort: '-createdAt',
+  callerId: null,
+  currentAccount: null,
+  accountContent: computed(function () {
+    return this.store.query('membership', {
+      'filter[is-active]': true,
+      sort: '-createdAt',
+      include: 'account',
+    }).then((memberships) => {
+      this.set('currentAccount', memberships.get('firstObject'));
+      return memberships;
+    });
+  }),
+  callerIdContent: computed(function () {
+    return DS.PromiseObject.create({
+      promise: this.get('store').query('caller-id', {
+        'page[offset]': 0,
+        'page[limit]': 100,
+        'filter[status]': 'success',
+        sort: '-created-at',
+      }),
+    });
+  }),
+  setAccountKeyForReqHeaders: observer('currentAccount', function () {
+    // setting the accountKey to be used within any future requests headers as `X-Account-Key`
+    if (this.get('currentAccount')) {
+      config.accountKey = this.get('currentAccount.account.key');
+    }
+  }),
+  // fed into ember-tabular to filter the table
+  staticParams: computed(function () {
+    return {
+      include: 'result,createdBy',
+    };
+  }),
+  columnOrder: ['from', 'to', 'direction', 'result.kind', 'createdBy.firstName', 'createdAt'],
+  listDirection: [
+    {
+      label: 'Inbound & Outbound',
+      value: 'inbound-outbound',
+    },
+    {
+      label: 'Inbound',
+      value: 'inbound',
+    },
+    {
+      label: 'Outbound',
+      value: 'outbound',
+    },
+  ],
+  actions: {
+    makeCall() {
+      // create call object/record
+      let call = this.get('store').createRecord('call', {
+        to: this.get('to'),
+        from: this.get('from'),
+        action: 'dial',
+        callerId: this.get('callerId'),
+      });
+      // save call record making request to API
+      call.save().then(
+        (record) => {
+          this.setProperties({
+            isSuccess: true,
+            successMessage: 'Success! Call was made',
+          });
+        },
+        (error) => {
+          // error
+          this.setProperties({
+            isFailure: true,
+            errors: get(error, 'errors'),
+            failureMessage: 'Error! Call was not successful',
+          });
+        }
+      );
+    },
+  },
+});

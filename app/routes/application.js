e68fea8d3382a3d5d4d2b14335e81b63d41aec71
Aaron Fischer <aaron.fischer@caxiam.com>
2018-02-21 15:28:57 -0500

    Slim down application route, logic can be handled within index controller

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,23 +1,7 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import SecureDefaultRouteFactory from 'voice-example-emberjs/mixins/routes/secure-default-route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-import config from 'voice-example-emberjs/config/environment';
 
 Route.reopenClass(SecureDefaultRouteFactory);
 
-export default Route.extend(ApplicationRouteMixin, {
-  session: service('session'),
-  sessionAuthenticated() {
-    this._super();
-    this.setupAccount(this.controller);
-  },
-  setupController(controller) {
-    if (this.get('session.isAuthenticated')) {
-      this.setupAccount(controller);
-    }
-  },
-  setupAccount(controller) {
-    // setup the account
-  },
-});
+export default Route.extend(ApplicationRouteMixin);

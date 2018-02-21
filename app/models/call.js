52258940f3d8bab2c8031e252c0c592cff0df872
Aaron Fischer <aaron.fischer@caxiam.com>
2018-02-21 15:36:28 -0500

    Remove unncessary attributes from models

--- app/models/call.js
+++ app/models/call.js
@@ -6,25 +6,12 @@ const attr = DS.attr;
 export default DS.Model.extend({
   parentCall: DS.belongsTo('call', { async: true, inverse: null }),
   callerId: DS.belongsTo('caller-id', { async: true, inverse: null }),
-  // envelope: DS.belongsTo('envelope', { async: true, inverse: 'call' }),
-  // result: DS.belongsTo('call-result', { async: true, inverse: null }),
-  // contact: DS.belongsTo('contact', { async: true, inverse: null }),
-  // recordedFile: DS.belongsTo('file', { async: true }),
 
   // will not force a FETCH to grab full record
   // will reference id already within the local store instead
   parentCallId: computed('parentCall', function () {
     return this.belongsTo('parentCall').id();
   }),
-  // envelopeId: computed('envelope', function () {
-  //   return this.belongsTo('envelope').id();
-  // }),
-  // recordedFileId: computed('recordedFile', function () {
-  //   return this.belongsTo('recordedFile').id();
-  // }),
-  // contactId: computed('contact', function () {
-  //   return this.belongsTo('contact').id();
-  // }),
 
   to: attr('phoneFormat'),
   from: attr('phoneFormat'),
--- mirage/models/call.js
+++ mirage/models/call.js
@@ -2,9 +2,5 @@ import { Model, belongsTo } from 'ember-cli-mirage';
 
 export default Model.extend({
   callerId: belongsTo({ inverse: null }),
-  // contact: belongsTo(),
   parentCall: belongsTo('call', { inverse: null }),
-  // envelope: belongsTo({ inverse: null }),
-  // result: belongsTo('call-result', { inverse: null }),
-  // recordedFile: belongsTo('file'),
 });

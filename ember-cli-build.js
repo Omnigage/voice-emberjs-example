b8795322e56f10bbe70c2709a3232a6dd118f678
Aaron Fischer <aaron.fischer@caxiam.com>
2018-02-22 21:05:17 -0500

    Setup intl-tel-input to support international phone numbers

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,8 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const EmberAppConfig = require('./config/environment.js');
+let Funnel = require('broccoli-funnel');
+let mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
 
@@ -26,5 +28,18 @@ module.exports = function(defaults) {
     }
   });
 
-  return app.toTree();
+  // Copy only the relevant files:
+  var internationalTelImages = new Funnel('node_modules/intl-tel-input', {
+   srcDir: 'build/img',
+   include: ['*.png'],
+   destDir: '/assets/images/intl-tel-input'
+  });
+
+  // International Telephone Input - https://github.com/Bluefieldscom/intl-tel-input
+  app.import('node_modules/intl-tel-input/build/js/intlTelInput.min.js');
+  app.import('node_modules/intl-tel-input/build/css/intlTelInput.css');
+  // International Telephone Input - libphonenumber utils
+  app.import('node_modules/intl-tel-input/build/js/utils.js');
+
+  return mergeTrees([app.toTree(), internationalTelImages], {overwrite: true});
 };
